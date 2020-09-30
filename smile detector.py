1cc4c21711f47ab1a2c17d222d545019ad65792e
dhruvtongia <dhruvtongia2000@gmail.com>
2020-09-30 21:54:20 +0530

    smile detector

---
+++ smile detector.py
@@ -0,0 +1,39 @@
+# Homework Solution
+
+# Importing the libraries
+import cv2
+
+# Loading the necessary cascades
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
+
+# Defining a function that will do the detections
+# We create a function that takes as input the image in black and white (gray) and the original image (frame), and that will return the same image with the detector rectangles. 
+def detect(gray, frame):# We apply the detectMultiScale method from the face cascade to locate one or several faces in the image.
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)# We paint a rectangle around the face.
+        roi_gray = gray[y:y+h, x:x+w] # We get the region of interest in the black and white image.
+        roi_color = frame[y:y+h, x:x+w] # We get the region of interest in the colored image.
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 18)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2) # We paint a rectangle around the eyes, but inside the referential of the face.
+        smiles = smile_cascade.detectMultiScale(roi_gray, 1.7, 22)
+        for (sx, sy, sw, sh) in smiles:
+            cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (200, 200, 0), 2) # We paint a rectangle around the smile, but inside the referential of the face.
+    return frame
+
+# Doing some Face Recognition with the webcam
+    
+video_capture = cv2.VideoCapture(0)# We turn the webcam on.
+while True:
+    _, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    canvas = detect(gray, frame)
+    cv2.imshow('Video', canvas)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release() # We turn the webcam off.
+cv2.destroyAllWindows() # We destroy all the windows inside which the images were displayed.
